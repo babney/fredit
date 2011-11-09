a00ba232de84be61bfbf86a2cb5513a0252a4c18
Bill Abney <bill.abney@gmail.com>
2011-11-08 20:35:53 -0800

    objects need to be instantiated. derp.

--- lib/fredit/backend.rb
+++ lib/fredit/backend.rb
@@ -1,19 +1,19 @@
 module Fredit
   class Backend
     def self.create(path)
-      raise StandardError("Subclass me and override this method")
+      raise StandardError.new("Subclass me and override this method")
     end
     def self.read(path, author)
-      raise StandardError("Subclass me and override this method")
+      raise StandardError.new("Subclass me and override this method")
     end
     def self.update(path, content, edit_message, author)
-      raise StandardError("Subclass me and override this method")
+      raise StandardError.new("Subclass me and override this method")
     end
     def self.upload(uploaded_file, author)
-      raise StandardError("Subclass me and override this method")
+      raise StandardError.new("Subclass me and override this method")
     end
     def self.delete(path)
-      raise StandardError("Subclass me and override this method")
+      raise StandardError.new("Subclass me and override this method")
     end
   end
 end
